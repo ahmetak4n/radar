b8d896f7146cad3e79998a2af21f39401d943383
ahmetak4n <ahmetak4n@gmail.com>
2025-03-17 15:33:47 +0300

    enterprise-search function updated

--- internal/network/network.go
+++ internal/network/network.go
@@ -6,9 +6,24 @@ import (
 	"io"
 	"net"
 	"net/http"
-	"sync"
 	"time"
 )
+
+// Check target host is accessible or not
+func HostConnection(ip string, port int) (net.Conn, error) {
+	var err error
+
+	connection, err := net.DialTimeout("tcp", net.JoinHostPort(ip, fmt.Sprint(port)), 10*time.Second)
+	if err != nil {
+		err = fmt.Errorf("network.HostConnection ::: %w", err)
+	}
+
+	if connection == nil {
+		err = fmt.Errorf("network.HostConnection ::: null.connection ::: %w", err)
+	}
+
+	return connection, err
+}
 
 // Prepare request with user supplied variables
 func PrepareRequest(requestMethod RequestMethod, url, payload string) (*http.Request, error) {
@@ -30,6 +45,7 @@ func PrepareRequest(requestMethod RequestMethod, url, payload string) (*http.Req
 }
 
 // Send reqeust to target
+// Return response body, status code and HTTP response headers
 func SendRequest(request *http.Request) ([]byte, int, http.Header, error) {
 	var err error
 
@@ -55,40 +71,4 @@ func SendRequest(request *http.Request) ([]byte, int, http.Header, error) {
 	}
 
 	return body, statusCode, headers, err
-}
-
-// Check target host is accessible or not
-func HostConnection(ip string, port int) (net.Conn, error) {
-	var err error
-
-	connection, err := net.DialTimeout("tcp", net.JoinHostPort(ip, fmt.Sprint(port)), 10*time.Second)
-	if err != nil {
-		err = fmt.Errorf("network.HostConnection ::: %w", err)
-	}
-
-	if connection == nil {
-		err = fmt.Errorf("network.HostConnection ::: null.connection ::: %w", err)
-	}
-
-	return connection, err
-}
-
-func RequestPool(numOfWorker int, totalRequest int) chan *http.Request {
-	var wg sync.WaitGroup
-
-	wg.Add(totalRequest)
-	jobs := make(chan *http.Request, totalRequest)
-
-	for i := 0; i < numOfWorker; i++ {
-		go requestWorker(jobs, &wg)
-	}
-
-	return jobs
-}
-
-func requestWorker(jobs chan *http.Request, wg *sync.WaitGroup) {
-	for job := range jobs {
-		SendRequest(job)
-		wg.Done()
-	}
 }
